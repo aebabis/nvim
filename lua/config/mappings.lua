8c24ecd9a7e996b2929b8e9eb99133bf74a0c5c0
Alexandra Babis <aebabis@gmail.com>
2024-11-05 16:15:09 -0500

    Adding keymapping for quick terminal

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,5 +1,8 @@
 -- Jump between splits with Ctrl + direction
-vim.keymap.set('n', '<C-h>', '<C-w>h');
-vim.keymap.set('n', '<C-j>', '<C-w>j');
-vim.keymap.set('n', '<C-k>', '<C-w>k');
-vim.keymap.set('n', '<C-l>', '<C-w>l');
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+-- Quick terminal
+vim.keymap.set('n', '<leader>T', '<C-w>s<C-w>j:terminal<enter>A', {desc='Open terminal'})
