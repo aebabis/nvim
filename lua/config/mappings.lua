6d7428750fc4a9614b5461bc65d59057be59352d
Alexandra Babis <aebabis@gmail.com>
2024-11-14 15:25:16 -0500

    Autosizing new window

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -14,9 +14,20 @@ local make_window = function(options)
   return handle
 end
 
+local get_new_win_height = function()
+	local session_height = vim.api.nvim_win_get_height(0)
+	local preferred_height = math.floor(session_height / 4)
+	if (session_height < 4) then
+		return 2
+	elseif (session_height < 16) then
+		return math.floor(session_height / 2)
+	else
+		return math.max(8, math.min(preferred_height, 24))
+	end
+end
+
 local open_split_terminal = function()
-  make_window({ split = 'below', win = -1 })
-  -- TODO: Resize window based on editor size
+  make_window({ split = 'below', win = -1, height = get_new_win_height() })
   vim.cmd(':terminal')
 end
 
